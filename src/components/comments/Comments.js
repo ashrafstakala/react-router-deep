2c87fddd11509c9328ef27f7c82c87d49bbf24d4
ashrafstakala <ashrafstakala@gmail.com>
2022-01-20 22:05:19 +0200

    fix new comments not loading from http

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -14,6 +14,7 @@ const NewCommentForm = (props) => {
 
   useEffect(() => {
     if (status === 'completed' && !error) {
+      onAddedComment();
     }
   }, [status, error, onAddedComment]);
 
